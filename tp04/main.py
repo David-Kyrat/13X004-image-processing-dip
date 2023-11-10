8c17a32a5193cd0d454d0d8f4859eabdf53f8c04
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-11-10 14:04:22 +0100

    Update

---
+++ tp04/main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    plt.plot([1, 2, 3], [999, 125, 576])
+    plt.show()
+    print_hi('PyCharm, and welcome to the world of Python!')
+
